af1d226c6843b3027dddc35d74ca8f7b3198e1b2
robsonf <robsonf@gmail.com>
2014-10-14 19:04:35 -0300

    atualizacao para experimentos

---
+++ src/TesteLista.java
@@ -0,0 +1,14 @@
+import java.io.Serializable;
+import java.util.ArrayList;
+
+
+public class TesteLista implements Serializable{
+	ArrayList<String> a;
+
+	public TesteLista(ArrayList<String> a) {
+		super();
+		this.a = a;
+	}
+	
+	
+}
